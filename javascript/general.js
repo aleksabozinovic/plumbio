37bfa7ecf4a2c6495d101e8e47a7bf777b65e17f
aleksabozinovic <aleksabozinovic722@gmail.com>
2023-04-15 23:15:07 +0200

    commit

--- javascript/general.js
+++ javascript/general.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector(".open--navigation--desktop");
 const overlay = document.querySelector(".overlay");
 const mobileNav = document.querySelector(".mobile--nav");
-
+const nav = document.querySelector("nav");
 burger.addEventListener("click", () => {
   overlay.classList.add("active");
   mobileNav.classList.add("active");
@@ -11,3 +11,26 @@ overlay.addEventListener("click", () => {
   overlay.classList.remove("active");
   mobileNav.classList.remove("active");
 });
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector(".first--section");
+const navHeight = nav.getBoundingClientRect().height;
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > navHeight ||
+    document.documentElement.scrollTop > navHeight
+  ) {
+    nav.classList.add("sticky");
+    document.querySelector(".first--section").style.paddingTop = `240px`;
+  } else {
+    nav.classList.remove("sticky");
+    document.querySelector(".first--section").style.paddingTop = `140px`;
+  }
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
